e4c4218e783b01739bfbd5fb3e31bf20b59851c7
Adél Mészáros <adel.meszaros@yahoo.com>
2019-11-04 11:37:46 +0200

    #3 implement developer class

---
+++ app/src/main/java/com/example/planningpoker/Developer.java
@@ -0,0 +1,44 @@
+package com.example.planningpoker;
+
+import java.util.ArrayList;
+
+public class Developer {
+
+    public static final String TABLE_NAME = "developers";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_DEVELOPER = "developer";
+    public static final String CREATE_TABLE = "CREATE TABLE" + TABLE_NAME + "("
+            + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_DEVELOPER + " TEXT"
+            + ")";
+
+    private int id;
+    private String developer;
+
+    public Developer(){
+    }
+
+    public  Developer(int id, String developer){
+        this.id=id;
+        this.developer=developer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+
+
+}
