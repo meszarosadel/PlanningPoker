f316251994a55f918f15f97bfd92dd7d1f81e9e8
Adél Mészáros <adel.meszaros@yahoo.com>
2019-11-04 14:52:57 +0200

    #4 implement RatingTasks class

---
+++ app/src/main/java/com/example/planningpoker/RatingTasks.java
@@ -0,0 +1,57 @@
+package com.example.planningpoker;
+
+public class RatingTasks {
+
+    private int id;
+    private int developer_id;
+    private int task_id;
+    private int  part;
+
+    public static final String TABLE_NAME = "ratingTasks";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_DEVELOPER_ID = "developer_id";
+    public static final String COLUMN_TASK_ID = "task_id";
+    public static final String COLUMN_PART = "part";
+
+    public RatingTasks(){
+    }
+
+    public RatingTasks(int id, int developer_id, int task_id,int part){
+        this.id = id;
+        this.developer_id = developer_id;
+        this.task_id = task_id;
+        this.part = part;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDeveloper_id() {
+        return developer_id;
+    }
+
+    public int getTask_id() {
+        return task_id;
+    }
+
+    public int getPart() {
+        return part;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDeveloper_id(int developer_id) {
+        this.developer_id = developer_id;
+    }
+
+    public void setTask_id(int task_id) {
+        this.task_id = task_id;
+    }
+
+    public void setPart(int part) {
+        this.part = part;
+    }
+}
