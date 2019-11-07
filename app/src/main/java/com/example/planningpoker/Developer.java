cc338f18f9ae5a520f94d006671e6ed2e39f6409
Adél Mészáros <adel.meszaros@yahoo.com>
2019-11-07 18:06:32 +0200

    #11 insert password column in Developer table

--- app/src/main/java/com/example/planningpoker/Developer.java
+++ app/src/main/java/com/example/planningpoker/Developer.java
@@ -7,20 +7,24 @@ public class Developer {
     public static final String TABLE_NAME = "developers";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_DEVELOPER = "developer";
+    public  static final String COLUMN_PASSWORD = "password";
     public static final String CREATE_TABLE = "CREATE TABLE" + TABLE_NAME + "("
             + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COLUMN_DEVELOPER + " TEXT"
+            + COLUMN_DEVELOPER + " TEXT,"
+            + COLUMN_PASSWORD + " TEXT"
             + ")";
 
     private int id;
     private String developer;
+    private String password;
 
     public Developer(){
     }
 
-    public  Developer(int id, String developer){
-        this.id=id;
-        this.developer=developer;
+    public  Developer(int id, String developer, String password){
+        this.id = id;
+        this.developer = developer;
+        this.password = password;
     }
 
     public int getId() {
@@ -29,6 +33,14 @@ public class Developer {
 
     public String getDeveloper() {
         return developer;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setId(int id) {
